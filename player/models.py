740da09f050507fc3abf8630ad7a0f594589e449
Valder Gallo <valdergallo@gmail.com>
2016-04-03 11:13:51 -0300

    add player app

---
+++ player/models.py
@@ -0,0 +1,53 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Server(models.Model):
+    name = models.CharField(blank=True, max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class SpecType(models.Model):
+    action = models.CharField(blank=True, max_length=100)
+
+    def __str__(self):
+        return self.action
+
+
+class Specs(models.Model):
+    image = models.ImageField(upload_to="/specs/", null=True, blank=True)
+    name = models.CharField(blank=True, max_length=100)
+    spec_type = models.ForeignKey(SpecType)
+
+    def __str__(self):
+        return self.name
+
+
+class Classes(models.Model):
+    image = models.ImageField(upload_to="/classes/", null=True, blank=True)
+    name = models.CharField(blank=True, max_length=100)
+    color = models.CharField(blank=True, max_length=100)
+    specs = models.ManyToManyField(Specs)
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    image = models.ImageField(upload_to="/player/%Y/%m/%d/", null=True, blank=True)
+    name = models.CharField(blank=True, max_length=100)
+    ilvl = models.IntegerField(blank=True, null=True)
+    lvl = models.IntegerField(blank=True, null=True)
+    server = models.ForeignKey(Server)
+    classe = models.ForeignKey(Classes)
+    main_spec = models.ForeignKey(Specs)
+    off_spec = models.ForeignKey(Specs)
+    creation_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return '{name} {ilvl}'.format(self.name, self.ilvl)
